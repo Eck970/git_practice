5ddd76707397e4f74d6ac434a0ef74222c207c77
Erik Khachaturyan <erikkhachaturyan@Eriks-MacBook-Air.local>
2023-08-10 18:19:49 +0400

    Minor test filed

---
+++ test.js
@@ -0,0 +1,13 @@
+let liza = [];
+
+const countingLiza = (n) => {
+    for (let i = 0; i<= n; i++){
+        console.log(`This is the ${i}-th iteration!`);
+    }
+}
+
+countingLiza(11);
+
+console.log("ola")
+
+
---
+++ test.py
@@ -0,0 +1,6 @@
+# A hello world function
+
+def hello():
+    print("hello world!")
+
+hello()
